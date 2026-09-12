70abdce86535b4cfb9aa0928d8514efbe88856dd
seungbeomcat <seocatgavin@gmail.com>
2026-09-12 20:50:00 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE="dosaljang-v112-20260912b";
+const STATIC_ASSETS=["./dosaljang-profile-20260912.png","./dosaljang-preview-20260912-v112-final.png","./manifest.webmanifest"];
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC_ASSETS)).catch(()=>{}));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener("fetch",e=>{
+  const r=e.request;
+  if(r.mode==="navigate"||r.destination==="document"){
+    e.respondWith(fetch(r,{cache:"no-store"}).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{
+    const cp=res.clone();
+    caches.open(CACHE).then(x=>x.put(r,cp)).catch(()=>{});
+    return res;
+  })));
+});
