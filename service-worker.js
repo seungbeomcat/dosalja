1127e03186d6248f9ef7c8ccb4116cb924149491
seungbeomcat <seocatgavin@gmail.com>
2026-09-12 23:21:34 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
-const CACHE="dosaljang-v112-20260912l";
+const CACHE="dosaljang-v112-20260912o";
 const STATIC_ASSETS=["./dosaljang-profile-v112-clean.png","./dosaljang-preview-20260912-v112-ui-final.png","./manifest.webmanifest"];
 self.addEventListener("install",e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC_ASSETS)).catch(()=>{}));
+ self.skipWaiting();
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC_ASSETS)).catch(()=>{}));
 });
 self.addEventListener("activate",e=>{
-  e.waitUntil(Promise.all([
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
-    self.clients.claim()
-  ]));
+ e.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+  self.clients.claim()
+ ]));
 });
 self.addEventListener("fetch",e=>{
-  const r=e.request;
-  if(r.mode==="navigate"||r.destination==="document"){
-    e.respondWith(fetch(r,{cache:"no-store"}).catch(()=>caches.match("./index.html")));
-    return;
-  }
-  e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{
-    const cp=res.clone();
-    caches.open(CACHE).then(x=>x.put(r,cp)).catch(()=>{});
-    return res;
-  })));
+ const r=e.request;
+ if(r.mode==="navigate"||r.destination==="document"){
+  e.respondWith(fetch(r,{cache:"no-store"}).catch(()=>caches.match("./index.html")));
+  return;
+ }
+ e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{
+  const cp=res.clone();
+  caches.open(CACHE).then(x=>x.put(r,cp)).catch(()=>{});
+  return res;
+ })));
 });
