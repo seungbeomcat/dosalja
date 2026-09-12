25e67d1afaa59248ef7177f0650eedfc63622ede
seungbeomcat <seocatgavin@gmail.com>
2026-09-12 21:12:20 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="dosaljang-v112-20260912c";
-const STATIC_ASSETS=["./dosaljang-profile-v112-final.png","./dosaljang-preview-20260912-v112-final2.png","./manifest.webmanifest"];
+const CACHE="dosaljang-v112-20260912d";
+const STATIC_ASSETS=["./dosaljang-profile-v112-clean.png","./dosaljang-preview-20260912-v112-clean.png","./manifest.webmanifest"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC_ASSETS)).catch(()=>{}));
